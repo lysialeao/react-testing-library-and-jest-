bdf7617945a9e59ccd54104fc9a8a76459fd5f15
lysia <lysia@evnts.com.br>
2023-04-03 17:44:58 -0300

    feat: :heavy_plus_sign: adding test libraries and configurations

---
+++ src/__test__/UserForm.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import { user } from '@testing-library/user-event'
+
+import UserForm from '../components/UserForm'
+
+test('it shows two inputs and a button', () => {
+  render(<UserForm />)
+  const inputs = screen.getAllByRole('textbox')
+  const button = screen.getByRole('button')
+  expect(inputs).toHaveLength(2)
+  expect(button).toBeInTheDocument()
+})
